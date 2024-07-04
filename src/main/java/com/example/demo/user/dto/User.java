a88266f38f15a6a44d2e01bc06d713fc89c655d6
wookyeong <dnwk203@naver.com>
2024-07-04 23:51:51 +0900

    [mod-Ing] - main html 수정

--- src/main/java/com/example/demo/user/dto/User.java
+++ src/main/java/com/example/demo/user/dto/User.java
@@ -1,4 +1,21 @@
 package com.example.demo.user.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Data
 public class User {
+
+    private String userName;
+    private String userId;
+    private String userPwd;
+    private String userPhoneNumber;
+    private String userEmail;
+    private String userAddress;
+    private String userGender;
+    private String userBirthday;
+    private int userAge;
+
 }

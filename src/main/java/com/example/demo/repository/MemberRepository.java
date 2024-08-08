ca7e9c853d87b3c527049d04357288a3a06c4324
wookyeong <dnwk203@naver.com>
2024-08-08 18:16:56 +0900

    [new-Ing] - 회원 도메인 개발 1. 회원 repository 개발 2. 회원 service 개발

---
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -0,0 +1,49 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository         // 스프링에서 제공하는 이 어노테이션을 사용하면 컴포넌트 스캔에 의해서 자동으로 Spring 빈으로 관리가 된다.
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    // @PersistenceContext  // spring이 생성한 entityManager를 주입받기 위해 작성한다. // 원래는 안되지만 spring boot가 @Autowire도 인젝션 지원해주기 때문에 최종적으로 이와같이 사용할 수 있다.
+    private final EntityManager em;
+
+//    public MemberRepository(EntityManager em) {
+//        this.em = em;
+//    }
+
+//    // 주석 : 직접 entityManageFactory를 사용하고 싶은 경우 아래와 같이 작성하여 사용하면 된다.
+//    @PersistenceUnit
+//    private EntityManagerFactory emf;
+
+
+    // 주석 : 회원 등록 기능
+    public void save(Member member) { em.persist(member); }
+
+    // 주석 : 회원 단건 조회 기능
+    // em.find(반환타입, PK)
+    public Member findOne(Long id) { return em.find(Member.class, id); }
+
+    // 주석 : 회원 전체 조회 기능
+    // JPQL을 작성한다.  => em.createQuery("JPQL 쿼리문", 반환 타입).getResultList();  리스트로 만들어서 조회하는 메서드
+    // JPQL과 SQL의 차이 SQL은 테이블 대상 JPQL은 엔티티 객체를 대상으로 쿼리가 실행된다.
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // 주석 : 회원 이름으로 조회 기능
+    // 조건문 작성시 파라미터로 넘겨받은 name을 :name으로 작성하여 setParameter메서드를 이용해 바인딩 시켜준다.
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,60 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service        // 스프링에서 제공하는 이 어노테이션을 사용하면 컴포넌트 스캔이 포함되어있어 스프링 빈으로 등록이 된다.
+@Transactional(readOnly = true) // 전체에 적용되지만 메서드 개별에도 적용 되어있으면 그것을 우선으로 한다.
+@RequiredArgsConstructor    // final이 있는 필드만 가지고 생성자를 만들어 준다. 자동으로 전부 생성자를 만들려면 @AllArgument 어노테이션을 사용하면 된다.
+public class MemberService {
+
+    // @Autowired  // 필드 인젝션 : 스프링이 빈에 등록되어있는 레포지토리를 등록해준다.
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();      // 저장된 id값이 뭔지 리턴해준다.
+    }
+
+
+    // 주석 : 중복 회원이 있는지 검사하는 로직
+    // 주의 : 멀티스레드의 상황을 고려해서 데이터베이스에 member의 name을 unique제약조건을 걸어주는게 좋은 방법이다.
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     * @param memberId
+     * @return
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
